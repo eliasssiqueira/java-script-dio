d2e27d0ae4e982561e58920de61823eb3a4405ea
Elias <eliassiqueira584@gmail.com>
2021-12-30 16:16:06 -0300

    arquivo com sintaxes de funcoes,constantes e variaveis

---
+++ sintaxe.js
@@ -0,0 +1,27 @@
+//usar duas barras para comentar linhas//
+/* usar "/*" para comentar
+varias linhas ou um bloco do código */
+
+ declarar variaveis: digitar "var nomeVariavel" ex:
+var idade;
+
+CONSTANTES:
+const nomeConstante=10;
+
+FUNCOES :
+
+function nomeFuncao(parametros){
+    return
+}
+ex:
+function soma(a,b){
+    return a+b;
+}
+
+
+CHAMAR FUNCAO NO CODIGO:
+
+soma(3,5);
+
+
+
