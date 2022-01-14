8784ea9e4440b2a000844b529e2ea17e5a1d2cce
Elias <eliassiqueira584@gmail.com>
2022-01-14 15:17:13 -0300

    ..

---
+++ sintaxe básica/script.js
@@ -0,0 +1,11 @@
+alert('Tudo certo');
+
+//vetores ou arrays
+
+//como declarar vetores
+/* let array = ['string',1,true];
+console.log(array); */
+
+//pode guardar varios tipos de dados 
+let array = ['string',1,true,['array1'],['array2']];
+console.log(array[3]);
